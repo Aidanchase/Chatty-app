594c5bdc66445ce15eb87624e5a071ebddfc2f1e
Aidanchase <aidanmro@gmail.com>
2019-07-02 01:49:13 +0000

    added styling and correct class => className

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,27 @@ import React, {Component} from 'react';
 <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
 </footer>); */}
 const Nav = props => {
-return (<nav class="navbar">
-<a href="/" class="navbar-brand">Chatty</a>
+return (<nav id="navbar">
+<a href="/" className="navbar-brand"><h1>Chatty</h1></a>
 </nav>);
     }
 
 const Main = props => {
-  return (<main class="messages">
-  <div class="message">
-    <span class="message-username">Anonymous1</span>
-    <span class="message-content">I won't be impressed with technology until I can download food.</span>
+  return (<main className="messages">
+  <div className="message">
+    <span className="message-username">Anonymous1</span>
+    <span className="message-content">I won't be impressed with technology until I can download food.</span>
   </div>
-  <div class="message system">
+  <div className="message-system">
     Anonymous1 changed their name to nomnom.
   </div>
   </main>);
 }
 
 const Footer = props => {
-  return (<footer class="chatbar">
-  <input class="chatbar-username" placeholder="Your Name (Optional)" />
-  <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
+  return (<footer id="chatbar">
+  <input className="chatbar-username" placeholder="Your Name (Optional)" />
+  <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
   </footer>);
 }
 
